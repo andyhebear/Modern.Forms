4321a06b1b561740712919b24f7977808d6e522f
RainsSoft <andyhebear@163.com>
2026-05-14 20:39:18 +0800

    update

--- src/Modern.Forms/Layout/PropertyNames.cs
+++ src/Modern.Forms/Layout/PropertyNames.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -61,7 +61,7 @@ internal sealed class PropertyNames
     public const string PreferredSize = "PreferredSize";
     public const string Padding = "Padding";
     public const string Parent = "Parent";
-    //public const string RightToLeft = "RightToLeft";
+    public const string RightToLeft = "RightToLeft";
     //public const string RightToLeftLayout = "RightToLeftLayout";
     //public const string RowHeadersVisible = "RowHeadersVisible";
     //public const string RowHeadersWidth = "RowHeadersWidth";
