0697c85d1c37d9bab1eaf3ad507df5555c460b04
RainsSoft <andyhebear@163.com>
2026-05-15 11:55:13 +0800

    Update IArrangedElement.cs

--- src/Modern.Forms/Layout/IArrangedElement.cs
+++ src/Modern.Forms/Layout/IArrangedElement.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -63,5 +63,5 @@ internal interface IArrangedElement : IComponent
     /// <summary>
     ///  Returns the element's children (on a control, this forwards to Controls)
     /// </summary>
-    IEnumerable<Control> Children { get; }
+    IList<Control> Children { get; }
 }
