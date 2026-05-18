479d0b8f4ed952b388b026ffc8e189a19ac086f2
RainsSoft <andyhebear@163.com>
2026-05-18 11:51:52 +0800

    add layout event when s_rightToLeftChangedEvent

--- src/Modern.Forms/Control.Events.cs
+++ src/Modern.Forms/Control.Events.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -42,6 +42,7 @@ public partial class Control
     private static readonly object s_tabStopChangedEvent = new object ();
     private static readonly object s_textChangedEvent = new object ();
     private static readonly object s_visibleChangedEvent = new object ();
+    private static readonly object s_rightToLeftChangedEvent = new object ();
 
     /// <summary>
     /// Raised when the AutoSize property is changed.
